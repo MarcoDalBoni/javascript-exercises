976cea39bdb6bf866a2c27c55981e3b4bfdfeda4
Marco Dal Boni <marco.dalboni@expandigroup.com>
2024-05-31 14:22:03 +0200

    tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+
+  let celsius = (farenheit - 32) * (5/9);
+  celsius = round(celsius, 1);
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  let farenheit = celsius * (9/5) + 32;
+  farenheit = round(farenheit, 1); 
+  return farenheit;
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
