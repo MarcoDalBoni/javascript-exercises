b1e9304cf244210c98d7a01ca46e95f7362ae543
Marco Dal Boni <marco.dalboni@expandigroup.com>
2024-05-31 14:44:29 +0200

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    if(index < 0 ) return 'OOPS'
+
+    let result = 0;
+    let prev = 0;
+
+    for(let i = 0; i < index; i++) {
+        if(result===0) {
+            result ++;
+        }
+        else {
+            result += prev;
+            prev = result - prev;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
