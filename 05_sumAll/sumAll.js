33f497722d4caed98ededa257d29f262c46a4149
Marco Dal Boni <marco.dalboni@expandigroup.com>
2024-05-31 12:49:38 +0200

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(start < 0 || end < 0 
+        || isNaN(start) || isNaN(end) 
+        || typeof(start) === "string" || typeof(end) === "string") {
+        return 'ERROR';
+    }
+
+    if(start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
