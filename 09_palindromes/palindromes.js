dc87e749498056fbfdca0309a799d168d97ac3e2
Marco Dal Boni <marco.dalboni@expandigroup.com>
2024-05-31 11:47:23 +0200

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz1234567890';
+
+    const clearStr = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
+
+    const reverseStr = clearStr.split('').reverse().join('');
+
+    return reverseStr === clearStr;
 };
 
 // Do not edit below this line
